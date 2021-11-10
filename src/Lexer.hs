1356d4305380630f9a8e4ff6d53c5efcd23b855b
Pierrick PROST <prost.pierrick@outlook.fr>
2021-11-10 11:28:49 +0100

    feature<lexer>: Really adding primitives (still WIP)

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -28,3 +28,28 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Boolean _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [Value] -> Value
+apply func args = maybe (Boolean False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [Value] -> Value)]
+primitives = [("+", numBinop (+)),
+            ("-", numBinop (-)),
+            ("*", numBinop (*)),
+            ("/", numBinop div),
+            ("mod", numBinop mod),
+            ("quotient", numBinop quot),
+            ("remainder", numBinop rem)]
+
+numBinop :: (Integer -> Integer -> Integer) -> [Value] -> Value
+numBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: Value -> Integer 
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in
+                            if null parsed
+                                then 0
+                                else fst $ head parsed
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
