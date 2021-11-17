5a17c7d20574dd6bccb3ad9e235f22f384abf01f
Pierrick PROST <prost.pierrick@outlook.fr>
2021-11-17 16:47:26 +0100

    feature<builtins>: Adding `atom?` builtins

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -12,6 +12,7 @@ import LispError
 import Unpack
 import Data.List
 import Lists
+import Foreign.C.Error (throwErrno)
 
 eval :: Value -> ThrowsError Value
 eval val@(String _) = Right val
@@ -21,6 +22,11 @@ eval cond@(List [Atom "if", pred, conseq, alt])
     | Right (Boolean False) <- eval pred = eval alt
     | Right (Boolean True) <- eval pred = eval conseq
     | otherwise = throwError $ BadSpecialForm "Unrecognized special form" cond
+eval cond@(List [Atom "atom?", expr])
+    | Right (Atom _) <- eval expr = Right (Boolean True)
+    | Right (List []) <- eval expr = Right (Boolean True)
+    | Right _ <- eval expr = Right (Boolean False)
+    | otherwise = throwError $ BadSpecialForm "Unrecognized  special form" cond
 eval (List [Atom "quote", val]) = Right val
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
